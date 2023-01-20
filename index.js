f52ced167aed0a7375622e6a5e751f5c3597e25a
Leahwillan <leah.willan@ada.ac.uk>
2023-01-20 15:09:24 +0000

    difference from answer function

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let num = 0;
 
 function generateRandomNumber() {
-  let random = Math.floor(Math.random() * 1000)
-  return(random)
+  let randomNum = Math.floor(Math.random() * 1000)
+  return(randomNum)
+}
+
+function differenceFromAnswer(guess,answer) {
+  if (guess > answer){
+    console.log("Too high")
+  } else if (guess < answer) {
+    console.log("Too low")
+  } else {
+    console.log("Correct!")
+  }
 }
 
 generateRandomNumber()
